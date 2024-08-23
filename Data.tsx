66932d679a5b987a6704d286f10256907c3fa885
vfffgrttgh <163544061+vfffgrttgh@users.noreply.github.com>
2024-08-23 20:36:31 +0530

    Update Data.tsx

--- Data.tsx
+++ Data.tsx
@@ -1,3 +1,5 @@
 // DATA BASE HERE
 
 export const name: string = "Your_name";
+
+// You can add and change the 'const' keyword to 'let' and add more data of different types like objects, arrays or numbers
